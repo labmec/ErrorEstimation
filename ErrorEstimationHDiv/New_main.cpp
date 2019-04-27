3e8090df218ca41a22c136d075bf899a62983b70
philippedevloo <philippedevloo1958@gmail.com>
2019-04-27 11:17:56 -0300

    Properly deleting the meshes

--- ErrorEstimationHDiv/New_main.cpp
+++ ErrorEstimationHDiv/New_main.cpp
@@ -56,8 +56,8 @@ int main(int argc, char *argv[]) {
     config.porder = 1;
     config.hdivmais = 1;
     config.makepressurecontinuous = true;
-    config.exact.fExact = TLaplaceExample1::EX;//ESinSin;//EArcTanSingular;//EArcTan;//ESinSinDirNonHom;//
-    config.problemname = "Const";//"ESinSin";//"ArcTang";//
+    config.exact.fExact = TLaplaceExample1::EX;//EArcTanSingular;//EArcTan;//ESinSinDirNonHom;//
+    config.problemname = "Linear";//"ESinSin";//"ArcTang";//
     
     TPZGeoMesh *gmesh = CreateGeoMesh();
         config.materialids.insert(1);
@@ -66,30 +66,25 @@ int main(int argc, char *argv[]) {
         config.gmesh = gmesh;
     
     
-    int nDiv=1;
+    int nDiv=4;
     
     UniformRefinement( nDiv,gmesh);
+
+//    {
+////        std::ofstream out("gmesh.vtk");
+////        TPZVTKGeoMesh::PrintGMeshVTK(gmesh, out);
+//        std::ofstream out2("gmeshInitial.txt");
+//        gmesh->Print(out2);
+//
+//    }
     
-
-
-
     
-    {
-        {
-            std::ofstream out("gmesh_b.vtk");
-            TPZVTKGeoMesh::PrintGMeshVTK(gmesh, out);
-            std::ofstream out2("gmeshInitial_b.txt");
-            gmesh->Print(out2);
-            
-        }
-        
-        
-        TPZManVector<TPZCompMesh*, 2> meshvec_HDiv(2, 0);
-        TPZCompMesh *cmesh_HDiv = CreateHDivMesh(config, meshvec_HDiv);//Hdiv x L2
-        cmesh_HDiv->InitializeBlock();
+    TPZManVector<TPZCompMesh*, 2> meshvec_HDiv(2, 0);
+    TPZCompMesh *cmesh_HDiv = CreateHDivMesh(config, meshvec_HDiv);//Hdiv x L2
+    cmesh_HDiv->InitializeBlock();
 
-        //cria malha hibrida
-      
+    //cria malha hibrida
+  
         TPZHybridizeHDiv hybrid;
         auto HybridMesh = hybrid.Hybridize(cmesh_HDiv, meshvec_HDiv);
         std::get<0>(HybridMesh)->CleanUpUnconnectedNodes();//enumerar adequadamente os connects
@@ -97,93 +92,58 @@ int main(int argc, char *argv[]) {
         delete cmesh_HDiv;
         delete meshvec_HDiv[0];
         delete meshvec_HDiv[1];
-        
-        
-        std::cout<<"---Original PerifericalMaterialId --- "<<std::endl;
-        std::cout <<" LagrangeInterface = "<<hybrid.fLagrangeInterface<<std::endl;
-        std::cout <<" HDivWrapMatid = "<<hybrid.fHDivWrapMatid<<std::endl;
-        std::cout <<" InterfaceMatid = "<<hybrid.fInterfaceMatid<<std::endl;
-        
-        
+    
+    int n=hybrid.fLagrangeInterface;
+    int n1=hybrid.fHDivWrapMatid;
+    int n2=hybrid.fInterfaceMatid;
+    
+    std::cout<<"---Original PerifericalMaterialId --- "<<std::endl;
+    std::cout <<" LagrangeInterface = "<<n<<std::endl;
+    std::cout <<" HDivWrapMatid = "<<n1<<std::endl;
+    std::cout <<" InterfaceMatid = "<<n2<<std::endl;
+    
+    
         cmesh_HDiv=std::get<0>(HybridMesh);//malha hribrida
         meshvec_HDiv[0] = std::get<1>(HybridMesh)[0];//malha Hdiv
         meshvec_HDiv[1] = std::get<1>(HybridMesh)[1];//malha L2
-        
-        {
-            std::ofstream out("gmesh_a.vtk");
-            TPZVTKGeoMesh::PrintGMeshVTK(gmesh, out);
-            std::ofstream out2("gmeshInitial_a.txt");
-            gmesh->Print(out2);
-            
-        }
-        
-        SolveHybridProblem(cmesh_HDiv);
-        PlotLagrangreMultiplier(meshvec_HDiv[1]);
-    }
-    // Criando mais uma malha hibrida
+    
+    SolveHybridProblem(cmesh_HDiv);
+    
     {
-//        TPZGeoMesh *gmesh = CreateGeoMesh();
-//        config.gmesh = gmesh;
-        
-        TPZManVector<TPZCompMesh*, 2> meshvec_HDiv(2, 0);
-        TPZCompMesh *cmesh_HDiv = CreateHDivMesh(config, meshvec_HDiv);//Hdiv x L2
-        cmesh_HDiv->InitializeBlock();
-        
-        //cria malha hibrida
-        
-        TPZHybridizeHDiv hybrid;
-        auto HybridMesh = hybrid.Hybridize(cmesh_HDiv, meshvec_HDiv);
-        std::get<0>(HybridMesh)->CleanUpUnconnectedNodes();//enumerar adequadamente os connects
-        std::get<0>(HybridMesh)->AdjustBoundaryElements();
-        delete cmesh_HDiv;
-        delete meshvec_HDiv[0];
-        delete meshvec_HDiv[1];
-        
-        
-        std::cout<<"---Original PerifericalMaterialId --- "<<std::endl;
-        std::cout <<" LagrangeInterface = "<<hybrid.fLagrangeInterface<<std::endl;
-        std::cout <<" HDivWrapMatid = "<<hybrid.fHDivWrapMatid<<std::endl;
-        std::cout <<" InterfaceMatid = "<<hybrid.fInterfaceMatid<<std::endl;
-        
-        
-        cmesh_HDiv=std::get<0>(HybridMesh);//malha hribrida
-        meshvec_HDiv[0] = std::get<1>(HybridMesh)[0];//malha Hdiv
-        meshvec_HDiv[1] = std::get<1>(HybridMesh)[1];//malha L2
-        
-        SolveHybridProblem(cmesh_HDiv);
-        PlotLagrangreMultiplier(meshvec_HDiv[1]);
-    
+//
+//        std::ofstream outgeo("HrybridGeometria.txt");
+//        std::get<0>(HybridMesh)->Reference()->Print(outgeo);
+//        std::ofstream out("OriginalHybridMesh.txt");
+//        std::get<0>(HybridMesh)->Print(out);
+//
+        std::ofstream out2("OriginalFluxMesh.txt");
+        meshvec_HDiv[0]->Print(out2);
+
+        std::ofstream out3("OriginalPotentialMesh.txt");
+        meshvec_HDiv[1]->Print(out3);
+
+
     }
-    
+
+    PlotLagrangreMultiplier(meshvec_HDiv[1]);
     
  
     //reconstroi potencial e calcula o erro
     {
-//        TPZManVector<TPZCompMesh *> MeshesHDiv(3);
-//        MeshesHDiv[0] = cmesh_HDiv;//malha hibrida
-//        MeshesHDiv[1] = meshvec_HDiv[0];//Hdiv
-//        MeshesHDiv[2] = meshvec_HDiv[1];//L2
-//
-////        MeshesHDiv[3] = meshvec_HDiv2[0];//Hdiv
-////        MeshesHDiv[4] = meshvec_HDiv2[1];//L2
-//        TPZHybridHDivErrorEstimator HDivEstimate(MeshesHDiv);
-//
-////        //----segunda malha
-////        TPZManVector<TPZCompMesh *> MeshesHDiv2(3);
-////        MeshesHDiv2[0] = cmesh_HDiv2;//malha hibrida
-////        MeshesHDiv2[1] = meshvec_HDiv2[0];//Hdiv
-////        MeshesHDiv2[2] = meshvec_HDiv2[1];//L2
-////        TPZHybridHDivErrorEstimator HDivEstimate2(MeshesHDiv2);
-//
-//
-//        HDivEstimate.fProblemConfig = config;
-//        HDivEstimate.fUpliftPostProcessMesh = 0;
-//        HDivEstimate.SetAnalyticSolution(&config.exact);
-//
-//        HDivEstimate.PotentialReconstruction();
-//
-//        TPZManVector<REAL> elementerrors;
-//        HDivEstimate.ComputeErrors(elementerrors);
+        TPZManVector<TPZCompMesh *> MeshesHDiv(3);
+        MeshesHDiv[0] = cmesh_HDiv;//malha hibrida
+        MeshesHDiv[1] = meshvec_HDiv[0];//Hdiv
+        MeshesHDiv[2] = meshvec_HDiv[1];//L2
+        TPZHybridHDivErrorEstimator HDivEstimate(MeshesHDiv);
+
+        HDivEstimate.fProblemConfig = config;
+        HDivEstimate.fUpliftPostProcessMesh = 0;
+        HDivEstimate.SetAnalyticSolution(&config.exact);
+        
+        HDivEstimate.PotentialReconstruction();
+        
+        TPZManVector<REAL> elementerrors;
+        HDivEstimate.ComputeErrors(elementerrors);
     }
     
     return 0;
@@ -225,9 +185,6 @@ void SolveHybridProblem(TPZCompMesh *Hybridmesh){
     direct = 0;
     an.Assemble();
     an.Solve();
-    
-    Hybridmesh->LoadSolution(Hybridmesh->Solution());
-    
     TPZStack<std::string> scalnames, vecnames;
     scalnames.Push("Pressure");
     vecnames.Push("Flux");
