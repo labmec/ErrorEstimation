5988bd46d50f57e44a09c2e0211c9ae46033f260
Gustavo <gustavoabatistela@gmail.com>
2019-06-03 15:45:52 -0300

    Derived Hdiv H1 class

---
+++ ErrorEstimationHDiv/TPZHDivErrorEstimatorH1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gustavo on 30/05/19.
+//
+
+#include "TPZHDivErrorEstimatorH1.h"
---
+++ ErrorEstimationHDiv/TPZHDivErrorEstimatorH1.h
@@ -0,0 +1,16 @@
+//
+// Created by gustavo on 30/05/19.
+//
+
+#include "TPZHybridHDivErrorEstimator.h"
+
+#ifndef ERRORESTIMATION_TPZHDIVERRORESTIMATORH1_H
+#define ERRORESTIMATION_TPZHDIVERRORESTIMATORH1_H
+
+
+class TPZHDivErrorEstimatorH1 : TPZHybridHDivErrorEstimator {
+
+};
+
+
+#endif //ERRORESTIMATION_TPZHDIVERRORESTIMATORH1_H
