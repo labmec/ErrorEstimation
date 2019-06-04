b90b9586678c3c81b505564517d37de5a1a153a9
philippedevloo <philippedevloo1958@gmail.com>
2019-06-04 15:32:00 -0300

    Implementing an error estimator for H(div) using H1 reconstruction

--- ErrorEstimationHDiv/TPZHDivErrorEstimatorH1.cpp
+++ ErrorEstimationHDiv/TPZHDivErrorEstimatorH1.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "TPZHDivErrorEstimatorH1.h"
+
+/// create the post processed multiphysics mesh (which is necessarily hybridized)
+void TPZHDivErrorEstimatorH1::CreatePostProcessingMesh()
+{
+    DebugStop();
+}
+
+/// compute a more precise approximation for the pressure
+void TPZHDivErrorEstimatorH1::UpliftPressure()
+{
+    
+}
