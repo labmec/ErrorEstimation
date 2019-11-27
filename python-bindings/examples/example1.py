a25752007f3b45e4fe0603d65df2d0d74fee1ef4
Gustavo <gustavoabatistela@gmail.com>
2019-11-27 17:17:32 -0300

    Updated class name

--- python-bindings/examples/example1.py
+++ python-bindings/examples/example1.py
@@ -11,16 +11,16 @@ val = cmesh.InsertMaterialObject(mat)
 
 D_Type = 0
 N_Type = 1
-val1 = TPZFMatrix(1, 1, 0.0)
-val2 = TPZFMatrix(1, 1, 0.0)
+val1 = TPZFMatrixReal(1, 1, 0.0)
+val2 = TPZFMatrixReal(1, 1, 0.0)
 bc_1 = mat.CreateBC(mat, 2, N_Type, val1, val2)
 cmesh.InsertMaterialObject(bc_1)
 
-val3 = TPZFMatrix(1, 1, 30.0)
+val3 = TPZFMatrixReal(1, 1, 30.0)
 bc_2 = mat.CreateBC(mat, 4, D_Type, val1, val3)
 cmesh.InsertMaterialObject(bc_2)
 
-val4 = TPZFMatrix(1, 1, 10.0)
+val4 = TPZFMatrixReal(1, 1, 10.0)
 bc_3 = mat.CreateBC(mat, 5, D_Type, val1, val4)
 cmesh.InsertMaterialObject(bc_3)
 
