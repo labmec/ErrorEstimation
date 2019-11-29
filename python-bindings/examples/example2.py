763e3d2353f8aab703a26355c31546de254bb172
Gustavo <gustavoabatistela@gmail.com>
2019-11-29 14:56:03 -0300

    Ongoing work on simulation

--- python-bindings/examples/example2.py
+++ python-bindings/examples/example2.py
@@ -1,6 +1,8 @@
 from neopz import *
 from errorestimation import *
 
+InitializePZLog()
+
 coord0 = TPZManVectorReal(3, 0.);
 coord1 = TPZManVectorReal(3, 1.);
 coord1[2] = 0.;
@@ -17,7 +19,14 @@ y_nel = 3;
 gmesh = Create2DGridMesh(x_nel, y_nel, coord0, coord1, bcIDs)
 PrintGMeshToVTK(gmesh, "GMeshBeforeRefinement.vtk")
 
-UniformRefinement(2, gmesh)
+#UniformRefinement(2, gmesh)
 PrintGMeshToVTK(gmesh, "GMeshAfterRefinement.vtk")
 
+cfg = ProblemConfig()
+cfg.gmesh = gmesh
+cfg.Porder = 1
+cfg.Hdivmais = 1
+multiphysicsCMesh = CreateHDivMesh(cfg)
+
+HybridizeCompMesh(multiphysicsCMesh)
 
