57135a7daaf9040a32663f6bf8f78f4b1af14c05
Gustavo Batistela <gustavoabatistela@gmail.com>
2020-11-10 15:28:08 -0300

    Indented code, made fNeumannLocalProblem false by default and added a new method to set it externally

--- ErrorEstimation/Material/TPZHDivErrorEstimateMaterial.h
+++ ErrorEstimation/Material/TPZHDivErrorEstimateMaterial.h
@@ -14,15 +14,11 @@
 #include <stdio.h>
 #include "mixedpoisson.h"
 
-
-class TPZHDivErrorEstimateMaterial : public TPZMixedPoisson
-{
+class TPZHDivErrorEstimateMaterial : public TPZMixedPoisson {
 
 public:
-    
-    
     TPZHDivErrorEstimateMaterial(int matid, int dim);
-    
+
     TPZHDivErrorEstimateMaterial();
     
     TPZHDivErrorEstimateMaterial(const TPZHDivErrorEstimateMaterial &copy);
@@ -32,39 +28,40 @@ public:
     virtual ~TPZHDivErrorEstimateMaterial();
     
     TPZHDivErrorEstimateMaterial &operator=(const TPZHDivErrorEstimateMaterial &copy);
-    
 
-    virtual void Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef) override;
-    
-     virtual void ContributeBC(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc) override;
-    
-    
-    virtual void FillDataRequirements(TPZVec<TPZMaterialData > &datavec) override;
-    virtual void FillBoundaryConditionDataRequirement(int type,TPZVec<TPZMaterialData > &datavec) override;
-    
+    virtual void Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek,
+                            TPZFMatrix<STATE> &ef) override;
+
+    virtual void ContributeBC(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek,
+                              TPZFMatrix<STATE> &ef, TPZBndCond &bc) override;
+
+    virtual void FillDataRequirements(TPZVec<TPZMaterialData> &datavec) override;
+    virtual void FillBoundaryConditionDataRequirement(int type, TPZVec<TPZMaterialData> &datavec) override;
+
     virtual void UpdateBCValues(TPZVec<TPZMaterialData> &datavec);
-    
-    
-    
-    
-    bool fNeumannLocalProblem = true;
 
-    virtual int NEvalErrors() override {return 5;}//erro de oscilacao de dados tbem
-    
+    bool fNeumannLocalProblem = false;
+
+    void SetNeumannProblem(bool neumannProblem) {
+        fNeumannLocalProblem = neumannProblem;
+    }
+
+    virtual int NEvalErrors() override { return 5; } // erro de oscilacao de dados tbem
+
     /// Compute the error and error estimate
     // error[0] - error computed with exact pressure
     // error[1] - error computed with reconstructed pressure
     // error[2] - energy error computed with exact solution
     // error[3] - energy error computed with reconstructed solution
-    virtual void Errors(TPZVec<TPZMaterialData> &data, TPZVec<STATE> &u_exact, TPZFMatrix<STATE> &du_exact, TPZVec<REAL> &errors) override;
-    
-void ErrorsBC(TPZVec<TPZMaterialData> &data, TPZVec<STATE> &u_exact, TPZFMatrix<STATE> &du_exact, TPZVec<REAL> &errors,TPZBndCond &bc)override;
-    
+    virtual void Errors(TPZVec<TPZMaterialData> &data, TPZVec<STATE> &u_exact, TPZFMatrix<STATE> &du_exact,
+                        TPZVec<REAL> &errors) override;
+
+    void ErrorsBC(TPZVec<TPZMaterialData> &data, TPZVec<STATE> &u_exact, TPZFMatrix<STATE> &du_exact,
+                  TPZVec<REAL> &errors, TPZBndCond &bc) override;
 
     virtual int VariableIndex(const std::string &name) override;
     virtual int NSolutionVariables(int var) override;
-    virtual void Solution(TPZVec<TPZMaterialData> &datavec, int var,
-                          TPZVec<STATE> &Solout) override;
+    virtual void Solution(TPZVec<TPZMaterialData> &datavec, int var, TPZVec<STATE> &Solout) override;
 
     // Returns the first non-null approximation space index, which will be the
     // H1 reconstruction space
