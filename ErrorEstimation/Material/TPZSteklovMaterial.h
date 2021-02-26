c4de432d0359994dd4e2b7c699fb2cff9a1add38
Gustavo Batistela <gustavoabatistela@gmail.com>
2021-02-26 15:44:50 -0300

    Remove undefined methods from TPZSteklovMaterial class

--- ErrorEstimation/Material/TPZSteklovMaterial.h
+++ ErrorEstimation/Material/TPZSteklovMaterial.h
@@ -5,13 +5,14 @@
 //  Created by Denise De Siqueira on 27/11/20.
 //
 /**
- This material implement the variational form for Steklov problem: find (sigma, u, lambda) such that
- sigma = -K grad u
- div(sigma)=0
- sigma.n = lamda u
+  This material implement the variational form for Steklov problem: find (sigma, u, lambda) such that:
 
- |A B^T||sigma|=Inverse(lambda)|C 0||sigma|
- |B 0  ||u    |                |0 0||u    |
+  sigma = -K grad u
+  div(sigma) = 0
+  sigma.n = lambda u
+
+  |A B^T||sigma| = Inverse(lambda)|C 0||sigma|
+  |B 0  ||u    |                  |0 0||u    |
 
  **/
 
@@ -30,38 +31,16 @@ public:
 
     TPZSteklovMaterial(const TPZSteklovMaterial &copy);
 
-    TPZSteklovMaterial(const TPZMixedPoisson &copy);
+    explicit TPZSteklovMaterial(const TPZMixedPoisson &copy);
 
-    virtual ~TPZSteklovMaterial();
+    ~TPZSteklovMaterial() override;
 
     TPZSteklovMaterial &operator=(const TPZSteklovMaterial &copy);
 
-    virtual void Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek,
-                            TPZFMatrix<STATE> &ef) override;
-    virtual void ContributeBC(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek,
-                              TPZFMatrix<STATE> &ef, TPZBndCond &bc) override;
-
-    virtual void FillDataRequirements(TPZVec<TPZMaterialData> &datavec) override;
-    virtual void FillBoundaryConditionDataRequirement(int type, TPZVec<TPZMaterialData> &datavec) override;
-
-    virtual void UpdateBCValues(TPZVec<TPZMaterialData> &datavec);
-
-    virtual int NEvalErrors() override { return 5; } // erro de oscilacao de dados tbem
-
-    /// Compute the error and error estimate
-    // error[0] - error computed with exact pressure
-    // error[1] - error computed with reconstructed pressure
-    // error[2] - energy error computed with exact solution
-    // error[3] - energy error computed with reconstructed solution
-    virtual void Errors(TPZVec<TPZMaterialData> &data, TPZVec<STATE> &u_exact, TPZFMatrix<STATE> &du_exact,
-                        TPZVec<REAL> &errors) override;
-
-    void ErrorsBC(TPZVec<TPZMaterialData> &data, TPZVec<STATE> &u_exact, TPZFMatrix<STATE> &du_exact,
-                  TPZVec<REAL> &errors, TPZBndCond &bc) override;
-
-    virtual int VariableIndex(const std::string &name) override;
-    virtual int NSolutionVariables(int var) override;
-    virtual void Solution(TPZVec<TPZMaterialData> &datavec, int var, TPZVec<STATE> &Solout) override;
+    void Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek,
+                    TPZFMatrix<STATE> &ef) override;
+    void ContributeBC(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef,
+                      TPZBndCond &bc) override;
 };
 
 #endif /* TPZSteklovMaterial_hpp */
