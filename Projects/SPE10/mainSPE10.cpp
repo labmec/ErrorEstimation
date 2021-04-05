d70785475b08773429b2fd0fe4d7d816fa1e4381
Gustavo Batistela <gustavoabatistela@gmail.com>
2021-04-05 18:49:55 -0300

    Create temporary 3D mesh

--- Projects/SPE10/mainSPE10.cpp
+++ Projects/SPE10/mainSPE10.cpp
@@ -12,15 +12,41 @@ TPZGeoMesh *CreateSPE10GeoMesh();
 void ReadSPE10CellPermeabilities(TPZFMatrix<REAL>* perm_mat);
 
 int main() {
-
     constexpr int n_cells = 60 * 220 * 85;
     auto perm_mat = std::make_unique<TPZFNMatrix<n_cells * 3, REAL>>(n_cells, 3);
     ReadSPE10CellPermeabilities(perm_mat.get());
 
-    //std::cout << "Creating SPE10 initial grid...\n";
-    //TPZGeoMesh *gmesh = CreateSPE10GeoMesh();
-    //Tools::PrintGeometry(gmesh, "SPE10Grid", false, true);
-    //std::cout << "SPE10 initial grid created. NElem: " << gmesh->NElements() << "\n";
+    REAL max_perm = 0.;
+    REAL min_perm = 1e15;
+    for (int i = 0; i < perm_mat->Rows(); i++) {
+        for (int j = 0; j < perm_mat->Cols(); j++) {
+            const REAL perm = perm_mat->operator()(i, j);
+            if (perm > max_perm) max_perm = perm;
+            if (perm < min_perm) min_perm = perm;
+        }
+    }
+
+    TPZGeoMesh *gmesh = CreateSPE10GeoMesh();
+
+    int n_cells2 = 0;
+
+    constexpr int c1 = 85;
+    constexpr int c2 = 220;
+    constexpr int c3 = 60;
+    for (int i = 0; i < c1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c3; k++) {
+                const int cell_id = c2 * c3 * i + c3 * j + k;
+                const double perm = perm_mat->operator()(cell_id, 0);
+                const double relative_perm = (perm - min_perm) / (max_perm - min_perm);
+                const int mat_id = static_cast<int>(std::round(255 * relative_perm));
+                gmesh->Element(cell_id)->SetMaterialId(mat_id);
+                n_cells2++;
+            }
+        }
+    }
+    Tools::PrintGeometry(gmesh, "SPE10Grid", false, true);
+    std::cout << "SPE10 initial grid created. NElem: " << gmesh->NElements() << "\n";
 
 
 
@@ -28,8 +54,10 @@ int main() {
 }
 
 TPZGeoMesh *CreateSPE10GeoMesh() {
+    std::cout << "Creating SPE10 initial grid...\n";
+
     const TPZManVector<REAL, 3> x0 = {0, 0, 0};
-    const TPZManVector<REAL, 3> x1 = {1200, 2200, 170};
+    const TPZManVector<REAL, 3> x1 = {1, 11./3, 85.};
     const TPZManVector<int, 3> ndiv = {60, 220, 85};
 
     constexpr std::array bcIDs = {-1, -1, -1, -1, -1, -1};
@@ -39,10 +67,14 @@ TPZGeoMesh *CreateSPE10GeoMesh() {
     TPZGeoMesh * gmesh = gen.BuildVolumetricElements(domainID);
     //TPZGeoMesh *gmesh = gen.BuildBoundaryElements(bcIDs[0], bcIDs[1], bcIDs[2], bcIDs[3], bcIDs[4], bcIDs[5]);
 
+    std::cout << "SPE10 initial grid created. NElem: " << gmesh->NElements() << "\n";
+
     return gmesh;
 }
 
 void ReadSPE10CellPermeabilities(TPZFMatrix<REAL>* perm_mat) {
+
+    std::cout << "Reading permeability data...\n";
 
     std::ifstream perm_file("InputData/spe_perm.dat", std::ios::in);
     if (!perm_file) {
@@ -50,7 +82,7 @@ void ReadSPE10CellPermeabilities(TPZFMatrix<REAL>* perm_mat) {
         DebugStop();
     }
 
-    int n_cells = perm_mat->Rows();
+    const int n_cells = perm_mat->Rows();
     int cell_id = 0;
     int coord_id = 0;
     int line_num = 0;
@@ -60,7 +92,6 @@ void ReadSPE10CellPermeabilities(TPZFMatrix<REAL>* perm_mat) {
         std::getline(perm_file, line, '\n');
         if (line.length() <= 1) continue;
         std::stringstream stream(line);
-        //std::cout << stream.str() << std::endl;
         if (cell_id == n_cells) {
             cell_id = 0;
             coord_id++;
