1c1d666d276d8ee9bfd47a822f48c95d01a39078
Gustavo Batistela <gustavoabatistela@gmail.com>
2021-04-06 11:20:41 -0300

    Start working with libInterpolate

--- Projects/SPE10/mainSPE10.cpp
+++ Projects/SPE10/mainSPE10.cpp
@@ -9,42 +9,52 @@
 #include <memory>
 #include <pzgmesh.h>
 
+#include <libInterpolate/Interpolate.hpp>
+typedef _2D::BicubicInterpolator<REAL> Interpolator;
+
+// Global variables
+constexpr int n_cells = 60 * 220 * 85;
+auto perm_mat = TPZFNMatrix<n_cells * 3, REAL>(n_cells, 3);
+Interpolator interpolator;
+
+// Function declarations
 TPZGeoMesh *CreateSPE10GeoMesh();
-void ReadSPE10CellPermeabilities(TPZFMatrix<REAL>* perm_mat);
+TPZCompMesh *CreateSPE10CompMesh(TPZGeoMesh *gmesh);
+void ReadSPE10CellPermeabilities();
+void PermeabilityFunction(const TPZVec<REAL> &x, TPZVec<REAL> &res, TPZFMatrix<REAL> &res_mat);
+void InsertMaterials(TPZCompMesh *cmesh);
 
 int main() {
-    constexpr int n_cells = 60 * 220 * 85;
-    auto perm_mat = std::make_unique<TPZFNMatrix<n_cells * 3, REAL>>(n_cells, 3);
-    ReadSPE10CellPermeabilities(perm_mat.get());
+    ReadSPE10CellPermeabilities();
 
     REAL max_perm = 0.;
     REAL min_perm = 1e15;
-    for (int i = 0; i < perm_mat->Rows(); i++) {
-        for (int j = 0; j < perm_mat->Cols(); j++) {
-            const REAL perm = perm_mat->operator()(i, j);
+    for (int i = 0; i < perm_mat.Rows(); i++) {
+        for (int j = 0; j < perm_mat.Cols(); j++) {
+            const REAL perm = perm_mat.operator()(i, j);
             if (perm > max_perm) max_perm = perm;
             if (perm < min_perm) min_perm = perm;
         }
     }
 
     TPZGeoMesh *gmesh = CreateSPE10GeoMesh();
-
-    constexpr int layer = 35;
-    constexpr int nx = 220;
-    constexpr int ny = 60;
-    for (int i = 0; i < nx; i++) {
-        for (int j = 0; j < ny; j++) {
-            const int cell_id = ny * i + j;
-            const double perm = perm_mat->operator()(cell_id + nx * ny * layer, 0);
-            const double relative_perm = (perm - min_perm) / (max_perm - min_perm);
-            const int mat_id = static_cast<int>(std::round(255 * relative_perm));
-            gmesh->Element(cell_id)->SetMaterialId(mat_id);
-        }
-    }
     Tools::PrintGeometry(gmesh, "SPE10Grid", false, true);
     std::cout << "SPE10 initial grid created. NElem: " << gmesh->NElements() << "\n";
 
+    //constexpr int layer = 35;
+    //constexpr int nx = 220;
+    //constexpr int ny = 60;
+    //for (int i = 0; i < nx; i++) {
+    //    for (int j = 0; j < ny; j++) {
+    //        const int cell_id = ny * i + j;
+    //        const double perm = perm_mat(cell_id + nx * ny * layer, 0);
+    //        const double relative_perm = (perm - min_perm) / (max_perm - min_perm);
+    //        const int mat_id = static_cast<int>(std::round(255 * relative_perm));
+    //        gmesh->Element(cell_id)->SetMaterialId(mat_id);
+    //    }
+    //}
 
+    TPZCompMesh *cmesh = CreateSPE10CompMesh(gmesh);
 
     return 0;
 }
@@ -72,7 +82,7 @@ TPZGeoMesh *CreateSPE10GeoMesh() {
     return gmesh;
 }
 
-void ReadSPE10CellPermeabilities(TPZFMatrix<REAL>* perm_mat) {
+void ReadSPE10CellPermeabilities() {
 
     std::cout << "Reading permeability data...\n";
 
@@ -82,7 +92,6 @@ void ReadSPE10CellPermeabilities(TPZFMatrix<REAL>* perm_mat) {
         DebugStop();
     }
 
-    const int n_cells = perm_mat->Rows();
     int cell_id = 0;
     int coord_id = 0;
     int line_num = 0;
@@ -98,9 +107,43 @@ void ReadSPE10CellPermeabilities(TPZFMatrix<REAL>* perm_mat) {
             if (coord_id > 2) DebugStop();
         }
         for (int i = 0; i < 6; i++) {
-            stream >> perm_mat->operator()(cell_id, coord_id);
+            stream >> perm_mat(cell_id, coord_id);
             cell_id++;
         }
     }
     std::cout << "Finished reading permeability data from input file!\n";
+}
+
+void PermeabilityFunction(const TPZVec<REAL> &x, TPZVec<REAL> &res, TPZFMatrix<REAL> &res_mat) {
+    res.resize(1);
+    res[0] = 1.5;
+}
+
+TPZCompMesh *CreateSPE10CompMesh(TPZGeoMesh *gmesh) {
+    TPZCompMesh *cmesh = new TPZCompMesh(gmesh);
+    int dim = gmesh->Dimension();
+    cmesh->SetDimModel(dim);
+
+    InsertMaterials(cmesh);
+
+}
+
+void InsertMaterials(TPZCompMesh *cmesh) {
+
+    auto *mix = new TPZMixedPoisson(1, cmesh->Dimension());
+
+    TPZFNMatrix<1, REAL> val1(1, 1, 0.), val2(1, 1, 0.);
+    constexpr int dirichlet_bc = 0;
+    constexpr int neumann_bc = 1;
+
+    // Zero flux (reservoir boundary)
+    TPZBndCond *zero_flux = mix->CreateBC(mix, -1, neumann_bc, val1, val2);
+    // Unit pressure at left reservoir boundary
+    val2(0, 0) = 1.;
+    TPZBndCond *pressure_left = mix->CreateBC(mix, -2, dirichlet_bc, val1, val2);
+
+    cmesh->InsertMaterialObject(mix);
+    cmesh->InsertMaterialObject(zero_flux);
+    cmesh->InsertMaterialObject(pressure_left);
+
 }
