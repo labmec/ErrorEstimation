e1e1fac15412430eb1d80cdd95162417f53622ce
gustavobat <gustavoabatistela@gmail.com>
2021-07-21 15:48:28 -0300

    Remove unused headers and functions from hdiv adaptivity main

--- Projects/ErrorEstimationHDiv/mainAdaptivity.cpp
+++ Projects/ErrorEstimationHDiv/mainAdaptivity.cpp
@@ -2,18 +2,13 @@
 // Created by Gustavo Batistela on 12/07/19.
 //
 
-#include "TPZRefPatternDataBase.h"
-#include "tpzarc3d.h"
-#include <cmath>
 #include "ProblemConfig.h"
 #include "TPZHDivErrorEstimator.h"
+#include "TPZRefPatternDataBase.h"
 #include "Tools.h"
 
 constexpr bool postProcessWithHDiv = false;
-constexpr int refinementSteps = 4;
-
-TPZMultiphysicsCompMesh *CreateHybridCompMesh(const ProblemConfig &config, TPZHybridizeHDiv &hybridizer);
-TPZGeoMesh *CreateLShapeGeoMesh(int nCoarseRef, int nInternalRef, TPZStack<int64_t> &mhmIndexes);
+constexpr int refinementSteps = 7;
 
 int main() {
 
@@ -74,57 +69,4 @@ int main() {
         }
     }
     return 0;
-}
-
-TPZGeoMesh *CreateLShapeGeoMesh(int nCoarseRef, int nInternalRef, TPZStack<int64_t> &mhmIndexes) {
-
-    TPZVec<int> bcIDs(8, -1);
-    TPZGeoMesh *gmesh = Tools::CreateQuadLShapeMesh(bcIDs);
-    gmesh->SetDimension(2);
-    gmesh->BuildConnectivity();
-
-    Tools::UniformRefinement(nCoarseRef, gmesh);
-    Tools::DivideLowerDimensionalElements(gmesh);
-
-    int64_t nElem = gmesh->NElements();
-    for (int64_t i = 0; i < nElem; i++) {
-        TPZGeoEl *gel = gmesh->Element(i);
-        if (gel->Dimension() != gmesh->Dimension() || gel->NSubElements() > 0) continue;
-        mhmIndexes.Push(i);
-    }
-
-    Tools::UniformRefinement(nInternalRef, gmesh);
-    Tools::DivideLowerDimensionalElements(gmesh);
-
-    for (int64_t i = 0; i < mhmIndexes.size(); i++) {
-        std::cout << mhmIndexes[i] << '\n';
-    }
-    std::cout << '\n';
-
-    return gmesh;
-}
-
-TPZMultiphysicsCompMesh *CreateHybridCompMesh(const ProblemConfig &config, TPZHybridizeHDiv &hybridizer) {
-
-    TPZMultiphysicsCompMesh *cmesh_HDiv = Tools::CreateMixedMesh(config); // Hdiv x L2
-
-#ifdef PZDEBUG
-    {
-        ofstream out("MixedMesh.txt");
-        cmesh_HDiv->Print(out);
-    }
-#endif
-
-    // Hybridize mesh
-    TPZMultiphysicsCompMesh* hybridMesh = hybridizer.Hybridize(cmesh_HDiv);
-    hybridMesh->CleanUpUnconnectedNodes(); // Enumereate connects correctly
-    hybridMesh->AdjustBoundaryElements();
-
-    delete cmesh_HDiv;
-
-    std::cout << "---Original PerifericalMaterialId --- " << std::endl;
-    std::cout << " LagrangeInterface = " << hybridizer.fLagrangeInterface << std::endl;
-    std::cout << " HDivWrapMatid = " << hybridizer.fHDivWrapMatid << std::endl;
-    std::cout << " InterfaceMatid = " << hybridizer.fInterfaceMatid << std::endl;
-    return hybridMesh;
 }
