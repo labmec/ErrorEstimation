fc5ec4fb4339df1e8ca7b87b1fe1e5b1727e08f4
gustavobat <gustavoabatistela@gmail.com>
2021-11-09 15:36:53 -0300

    Add RefTree struct and member var

--- Projects/SPE10/TPZMultiscaleGridGen2D.h
+++ Projects/SPE10/TPZMultiscaleGridGen2D.h
@@ -7,6 +7,27 @@
 
 #include <pzgmesh.h>
 
+struct RefTree {
+    int fActualNumber{0};
+    RefTree *fChildLeft = nullptr;
+    RefTree *fChildRight = nullptr;
+
+    RefTree() = default;
+
+    explicit RefTree(const int actual_number) {
+        fActualNumber = actual_number;
+        FillRefTree();
+    }
+
+    void FillRefTree() {
+        std::div_t division = std::div(fActualNumber, 2);
+        if (fActualNumber != 1) {
+            fChildLeft = new RefTree(division.quot);
+            fChildRight = new RefTree(division.quot + division.rem);
+        }
+    }
+};
+
 class TPZMultiscaleGridGen2D {
 
 private:
@@ -16,6 +37,7 @@ private:
     const TPZManVector<REAL, 3> fMinX{0};
     const TPZManVector<REAL, 3> fMaxX{0};
 
+    RefTree fRefTree;
     std::map<int, std::map<int, TPZRefPattern>> fRefPatterns;
 
     TPZGeoMesh *fGeoMesh = nullptr;
