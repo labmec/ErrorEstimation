a2f38076e7de41298288784d9a35cb958f43e89b
philippedevloo <phil@fec.unicamp.br>
2025-05-05 09:31:17 -0300

    Material to estimate the error using H1 hybrid

---
+++ ErrorEstimation/Material/TPZH1ErrorHybridH1EstimateMaterial.cpp
@@ -0,0 +1,8 @@
+//
+//  TPZH1ErrorHybridH1EstimateMaterial.cpp
+//  HybridH1vsMixed
+//
+//  Created by Philippe Devloo on 01/05/25.
+//
+
+#include "TPZH1ErrorHybridH1EstimateMaterial.hpp"
---
+++ ErrorEstimation/Material/TPZH1ErrorHybridH1EstimateMaterial.h
@@ -0,0 +1,13 @@
+//
+//  TPZH1ErrorHybridH1EstimateMaterial.hpp
+//  HybridH1vsMixed
+//
+//  Created by Philippe Devloo on 01/05/25.
+//
+
+#ifndef TPZH1ErrorHybridH1EstimateMaterial_hpp
+#define TPZH1ErrorHybridH1EstimateMaterial_hpp
+
+#include <stdio.h>
+
+#endif /* TPZH1ErrorHybridH1EstimateMaterial_hpp */
